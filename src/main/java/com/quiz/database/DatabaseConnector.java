355afd762edf6ec0933ddfef697ed31ff460361c
eminecetin <eminecetinn970@gmail.com>
2023-11-28 12:35:36 +0300

    emine/OC-8-create-databaseconnector-class

---
+++ src/main/java/com/quiz/database/DatabaseConnector.java
@@ -0,0 +1,25 @@
+package com.quiz.database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static final String URL = "jdbc:postgresql://localhost:5432/yourdatabase";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    public static void close(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+

2425c27e1bd12e2b7dd00576451afc6d4e76cfe1
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2023-11-30 21:52:41 +0300

    [OC-22]-created UserQuiz queries

--- src/main/java/com/quiz/repository/CreateTable.java
+++ src/main/java/com/quiz/repository/CreateTable.java
@@ -1,5 +1,6 @@
 package com.quiz.repository;
 
+
 import com.quiz.config.DBConnectorConfig;
 
 import java.sql.SQLException;
@@ -15,6 +16,18 @@ public class CreateTable {
                     "name VARCHAR(255))"; // SQL
             statement.executeUpdate(query);
             System.out.println("Person table created successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createUserQuiz(){
+        try (Statement statement = DBConnectorConfig.getConnection().createStatement()){
+            String query = "CREATE TABLE IF NOT EXISTS user_quiz(" +
+                    "id SERIAL PRIMARY KEY," +
+                    "name VARCHAR(255))";
+            statement.executeUpdate(query);
+            System.out.println("User Quiz table created successfully");
         } catch (SQLException e) {
             e.printStackTrace();
         }

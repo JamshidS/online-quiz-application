5b351d7289b52c78a09ac212eaf99b65f63a6af2
furkanErenoglu <99097997+furkanErenoglu@users.noreply.github.com>
2023-12-04 15:35:53 +0300

    [OC-22]queries updated

--- src/main/java/com/quiz/repository/CreateTable.java
+++ src/main/java/com/quiz/repository/CreateTable.java
@@ -25,7 +25,17 @@ public class CreateTable {
         try (Statement statement = DBConnectorConfig.getConnection().createStatement()){
             String query = "CREATE TABLE IF NOT EXISTS user_quiz(" +
                     "id SERIAL PRIMARY KEY," +
-                    "name VARCHAR(255))";
+                    "name VARCHAR(255),"+
+                    "uuid VARCHAR(255),"+
+                    "description VARCHAR(255),"+
+                    "instructions VARCHAR(255),"+
+                    "duration BIGINT,"+
+                    "attempts BIGINT,"+
+                    "difficulty VARCHAR(255),"+
+                    "status BOOLEAN,"+
+                    "created_at Date,"+
+                    "user_id BIGINT,"+
+                    "quiz_id BIGINT)";
             statement.executeUpdate(query);
             System.out.println("User Quiz table created successfully");
         } catch (SQLException e) {

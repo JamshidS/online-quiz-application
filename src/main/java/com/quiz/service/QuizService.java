dd9570fd5524632d453c3bc11efae0f0c8296a31
eminecetin <eminecetinn970@gmail.com>
2023-12-21 21:21:38 +0300

    [OC-23]-create-userservice

--- src/main/java/com/quiz/service/QuizService.java
+++ src/main/java/com/quiz/service/QuizService.java
@@ -1,4 +1,13 @@
 package com.quiz.service;
 
+import com.quiz.model.User;
+
+import java.util.List;
+
 public interface QuizService {
+    String createUser(User user);
+    List<User> getUsers();
+    User getUser(Long id) throws Exception;
+    String updateUser(Long id, User userDto) throws Exception;
+    String deleteUser(Long id) throws Exception;
 }
--- src/main/java/com/quiz/service/impl/QuizServiceImpl.java
+++ src/main/java/com/quiz/service/impl/QuizServiceImpl.java
@@ -1,6 +1,66 @@
 package com.quiz.service.impl;
 
+import com.quiz.model.User;
+import com.quiz.repository.UserRepository;
 import com.quiz.service.QuizService;
 
+import java.util.List;
+
+
 public class QuizServiceImpl implements QuizService {
+    private final UserRepository userRepository;
+
+    public QuizServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public String createUser(User user) {
+        try {
+            userRepository.addUser(user);
+            return "User created successfully.";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error creating user.";
+        }    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return userRepository.getUserById(id);
+    }
+
+    @Override
+    public String updateUser(Long id, User userDto) throws Exception {
+        User existingUser = userRepository.getUserById(id);
+
+        if (existingUser != null) {
+            existingUser.setEmail(userDto.getEmail());
+            existingUser.setFullName(userDto.getFullName());
+
+            User updatedUser = userRepository.updateUser(id, userDto.getEmail(), userDto.getFullName());
+
+            if (updatedUser != null) {
+                return "User updated successfully.";
+            } else {
+                return "Error updating user.";
+            }
+        } else {
+            return "User not found with ID: " + id;
+        }    }
+
+    @Override
+    public String deleteUser(Long id) throws Exception {
+        User existingUser = userRepository.getUserById(id);
+
+        if (existingUser != null) {
+            userRepository.deleteUser(id);
+            return "User deleted successfully.";
+        } else {
+            return "User not found with ID: " + id;
+        }    }
 }

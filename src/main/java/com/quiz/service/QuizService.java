2c7af7f703a40c642d4de3d81c8e91605cff7dbe
eycwave <emreyuce000@gmail.com>
2023-12-21 23:01:05 +0300

    [OC-26] - beginning

--- src/main/java/com/quiz/service/QuizService.java
+++ src/main/java/com/quiz/service/QuizService.java
@@ -1,4 +1,7 @@
 package com.quiz.service;
 
+import com.quiz.model.QuizMetaData;
+
 public interface QuizService {
+    String addQuizMetaDataToQuizQuestion(QuizMetaData quizMetaData);
 }
--- src/main/java/com/quiz/service/impl/QuizServiceImpl.java
+++ src/main/java/com/quiz/service/impl/QuizServiceImpl.java
@@ -1,6 +1,18 @@
 package com.quiz.service.impl;
 
+import com.quiz.model.QuizMetaData;
+import com.quiz.repository.QuizMetaDataRepository;
 import com.quiz.service.QuizService;
 
 public class QuizServiceImpl implements QuizService {
+    private final QuizMetaDataRepository quizMetaDataRepository;
+    public QuizServiceImpl(QuizMetaDataRepository quizMetaDataRepository){
+        this.quizMetaDataRepository = quizMetaDataRepository;
+    }
+    @Override
+    public String addQuizMetaDataToQuizQuestion(QuizMetaData quizMetaData) {
+        // idk how to write it right now. but i'll add...
+        return null;
+    }
 }
+

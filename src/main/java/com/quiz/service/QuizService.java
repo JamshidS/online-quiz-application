ca926ece038a267fd07c3ba978f3fc7235852985
eycwave <emreyuce000@gmail.com>
2023-12-25 21:43:41 +0300

    [OC-26] - relation established with QuizQuestion

--- src/main/java/com/quiz/service/QuizService.java
+++ src/main/java/com/quiz/service/QuizService.java
@@ -1,7 +1,10 @@
 package com.quiz.service;
 
 import com.quiz.model.QuizMetaData;
+import com.quiz.model.QuizQuestion;
+
+import java.util.List;
 
 public interface QuizService {
-    String addQuizMetaDataToQuizQuestion(QuizMetaData quizMetaData);
+    String addQuizMetaDataToQuizQuestion(List<QuizMetaData> quizMetaDataList, int quizQuestionID);
 }
--- src/main/java/com/quiz/service/impl/QuizServiceImpl.java
+++ src/main/java/com/quiz/service/impl/QuizServiceImpl.java
@@ -1,18 +1,47 @@
 package com.quiz.service.impl;
 
 import com.quiz.model.QuizMetaData;
+import com.quiz.model.QuizQuestion;
 import com.quiz.repository.QuizMetaDataRepository;
+import com.quiz.repository.QuizQuestionRepository;
 import com.quiz.service.QuizService;
+import java.util.*;
 
 public class QuizServiceImpl implements QuizService {
-    private final QuizMetaDataRepository quizMetaDataRepository;
-    public QuizServiceImpl(QuizMetaDataRepository quizMetaDataRepository){
+    private QuizMetaDataRepository quizMetaDataRepository;
+    private QuizQuestionRepository quizQuestionRepository;
+
+    public QuizServiceImpl(QuizMetaDataRepository quizMetaDataRepository, QuizQuestionRepository quizQuestionRepository){
         this.quizMetaDataRepository = quizMetaDataRepository;
+        this.quizQuestionRepository = quizQuestionRepository;
     }
+
     @Override
-    public String addQuizMetaDataToQuizQuestion(QuizMetaData quizMetaData) {
-        // idk how to write it right now. but i'll add...
-        return null;
+    public String addQuizMetaDataToQuizQuestion(List<QuizMetaData> quizMetaDataList, int quizQuestionID) {
+        try {
+            QuizQuestion quizQuestion = quizQuestionRepository.getQuizQuestionByID(quizQuestionID);
+
+            if (quizQuestion!=null) {
+                boolean trueFound = false;
+                for (QuizMetaData quizMetaData : quizMetaDataList) {
+                    if (quizMetaData.isCorrect()) {
+                        if (trueFound) {
+                            return "Only one 'True' value is allowed in QuizMetaData.";
+                        }
+                        trueFound = true;
+                    }
+                    quizMetaData.setQuizQuestion(quizQuestion);
+                    quizMetaDataRepository.save(quizMetaData);
+                }
+                return "QuizMetaData added successfully.";
+            } else {
+                return "QuizQuestion with ID " + quizQuestionID + " not found.";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "An error occurred while adding QuizMetaData.";
+        }
+
     }
 }
 

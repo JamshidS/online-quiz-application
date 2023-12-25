264dac7e6f54486a4bd6c5ee173099f5e5578423
eminecetin <eminecetinn970@gmail.com>
2023-12-25 19:54:53 +0300

    [OC-23]-create-userservice

---
+++ src/main/java/com/quiz/service/UserService.java
@@ -0,0 +1,13 @@
+package com.quiz.service;
+
+import com.quiz.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    String createUser(User user);
+    List<User> getUsers();
+    User getUser(Long id) throws Exception;
+    String updateUser(Long id, User userDto) throws Exception;
+    String deleteUser(Long id) throws Exception;
+}

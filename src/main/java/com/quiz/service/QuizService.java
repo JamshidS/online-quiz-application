33ac7a2ba2850496d6936cee6872a997b36946a9
eycwave <emreyuce000@gmail.com>
2024-01-08 19:56:35 +0300

    [OC-8] - edited quiz service methods

--- src/main/java/com/quiz/service/QuizService.java
+++ src/main/java/com/quiz/service/QuizService.java
@@ -1,6 +1,7 @@
 package com.quiz.service;
 
 
+import com.quiz.model.Quiz;
 import com.quiz.model.QuizMetaData;
 
 
@@ -11,24 +12,37 @@ import java.util.List;
 public interface QuizService {
 
     String addQuizQuestionToQuizMetaData(List<QuizMetaData> quizMetaDataList, int quizQuestionID);
+
     QuizMetaData getQuizMetaData(int quizMetaDataID);
+
     List<QuizMetaData> getAllQuizMetaData();
+
     List<QuizMetaData> getAllQuizMetaDataWithQuizQuestionID(int quizQuestionID);
-    String updateQuizMetaData(int quizQuestionID, int quizMetaDataID, QuizMetaData metaData);
+
+    String updateQuizMetaData(int quizQuestionID, int quizMetaDataID, QuizMetaData updatedMetaData);
+
     void deleteQuizMetaData(int quizMetaDataID);
 
-   String addQuizQuestion(QuizQuestion quizQuestion);
+    String saveQuizMetaData(int quizMetaDataID);
 
-   String updateQuizQuestion(Long questionId,String newQuestion);
+    QuizQuestion getQuizQuestion(int id);
 
-   String  deleteQuizQuestion(Long questionId);
+    List<QuizQuestion> getAllQuizQuestion();
 
-   List<QuizQuestion> getAllQuizQuestion();
+    String updateQuizQuestion(Long questionId, String newQuestion);
 
-   List<QuizQuestion> getAllQuizQuestionByQuizID(Long quizId);
+    String deleteQuizQuestion(Long questionId);
 
+    String addQuizQuestion(QuizQuestion quizQuestion);
 
+    Quiz getQuiz(int id);
 
+    List<Quiz> getAllQuiz();
 
+    Quiz updateQuiz(int id, Quiz updatedQuiz);
+
+    void deleteQuiz(int id);
+
+    String saveQuiz(Quiz quiz);
 }
 
